bb47c54ca63a7ea62ea5ecb9902c1444defc981d
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-09-13 16:55:59 +0530

    Add logic to gracefully handle error

--- project.py
+++ project.py
@@ -13,31 +13,34 @@ OUTPUT_FILENAME = "coupons.txt"
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-n",
-        type=int,
-        default=DEFAULT_NUMBER_OF_COUPONS,
-        help=f"Number of coupons to generate. [Min: {MIN_NUMBER_OF_COUPONS}, Max: {MAX_NUMBER_OF_COUPONS}, Default: {DEFAULT_NUMBER_OF_COUPONS}]",
-    )
-    parser.add_argument(
-        "-l",
-        type=int,
-        default=DEFAULT_NUMBER_OF_COUPON_CHARACTERS,
-        help=f"Number of characters in a coupon. [Min: {MIN_NUMBER_OF_COUPON_CHARACTERS}, Max: {MAX_NUMBER_OF_COUPON_CHARACTERS}, Default: {DEFAULT_NUMBER_OF_COUPON_CHARACTERS}]",
-    )
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "-n",
+            type=int,
+            default=DEFAULT_NUMBER_OF_COUPONS,
+            help=f"Number of coupons to generate. [Min: {MIN_NUMBER_OF_COUPONS}, Max: {MAX_NUMBER_OF_COUPONS}, Default: {DEFAULT_NUMBER_OF_COUPONS}]",
+        )
+        parser.add_argument(
+            "-l",
+            type=int,
+            default=DEFAULT_NUMBER_OF_COUPON_CHARACTERS,
+            help=f"Number of characters in a coupon. [Min: {MIN_NUMBER_OF_COUPON_CHARACTERS}, Max: {MAX_NUMBER_OF_COUPON_CHARACTERS}, Default: {DEFAULT_NUMBER_OF_COUPON_CHARACTERS}]",
+        )
+        args = parser.parse_args()
 
-    number_of_coupons = args.n
-    number_of_chars_in_a_coupon = args.l
+        number_of_coupons = args.n
+        number_of_chars_in_a_coupon = args.l
 
-    validate(number_of_coupons, number_of_chars_in_a_coupon)
+        validate(number_of_coupons, number_of_chars_in_a_coupon)
 
-    coupons = generate(number_of_coupons, number_of_chars_in_a_coupon)
+        coupons = generate(number_of_coupons, number_of_chars_in_a_coupon)
 
-    save(coupons)
+        save(coupons)
 
-    print(f"💾 Saved {number_of_coupons} coupon(s) in file {OUTPUT_FILENAME}")
+        print(f"💾 Saved {number_of_coupons} coupon(s) in file {OUTPUT_FILENAME}")
+    except Exception as e:
+        print(e)
 
 
 def validate(number_of_coupons: int, number_of_chars_in_a_coupon: int) -> None:
