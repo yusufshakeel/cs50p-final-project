c3997a879c36c34a3bde319c8fac516ca5b3a0c0
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-09-13 16:03:09 +0530

    Add tests for validate function

---
+++ test_project.py
@@ -0,0 +1,33 @@
+import pytest
+from project import validate
+
+def test_validate():
+    assert validate(1, 5) == None
+
+
+def test_validate_should_raise_ValueError_when_n_is_less_than_1():
+    with pytest.raises(ValueError) as e:
+        validate(-1, 10)
+
+    assert str(e.value) == "n cannot be less than 1"
+
+
+def test_validate_should_raise_ValueError_when_n_is_greater_than_10000():
+    with pytest.raises(ValueError) as e:
+        validate(100000, 10)
+
+    assert str(e.value) == "n cannot be greater than 10000"
+
+
+def test_validate_should_raise_ValueError_when_l_is_less_than_4():
+    with pytest.raises(ValueError) as e:
+        validate(10, 1)
+
+    assert str(e.value) == "l cannot be less than 4"
+
+
+def test_validate_should_raise_ValueError_when_l_is_greater_than_16():
+    with pytest.raises(ValueError) as e:
+        validate(10, 10000)
+
+    assert str(e.value) == "l cannot be greater than 16"
