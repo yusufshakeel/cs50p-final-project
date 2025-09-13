370b5d1c0bdbf53af5c2bc2544247d56067906c7
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-09-13 19:32:25 +0530

    Add prefix validation

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import argparse
 import random
 import os
+import re
 from typing import List, Set
 
 MIN_NUMBER_OF_COUPONS = 1
@@ -9,6 +10,8 @@ DEFAULT_NUMBER_OF_COUPONS = 1
 MIN_LENGTH_OF_COUPON = 4
 MAX_LENGTH_OF_COUPON = 16
 DEFAULT_LENGTH_OF_COUPON = 5
+MAX_LENGTH_OF_PREFIX = 16
+ALLOWED_CHARACTERS_FOR_PREFIX = "[A-Z0-9]"
 CHARACTER_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 OUTPUT_DIR = "output"
 OUTPUT_FILENAME = "coupons.txt"
@@ -37,6 +40,14 @@ def main():
             help=f"Length of coupon. [Min: {MIN_LENGTH_OF_COUPON}, Max: {MAX_LENGTH_OF_COUPON}, Default: {DEFAULT_LENGTH_OF_COUPON}]",
         )
         parser.add_argument(
+            "-p",
+            "--prefix",
+            dest="p",
+            type=str,
+            default="",
+            help=f"Prefix for the coupons",
+        )
+        parser.add_argument(
             "-s",
             "--save",
             dest="s",
@@ -47,8 +58,9 @@ def main():
 
         number_of_coupons = args.n
         number_of_chars_in_a_coupon = args.l
+        prefix = args.p
 
-        validate(number_of_coupons, number_of_chars_in_a_coupon)
+        validate(number_of_coupons, number_of_chars_in_a_coupon, prefix)
 
         coupons = generate(number_of_coupons, number_of_chars_in_a_coupon)
 
@@ -64,7 +76,9 @@ def main():
         print(e)
 
 
-def validate(number_of_coupons: int, number_of_chars_in_a_coupon: int) -> None:
+def validate(
+    number_of_coupons: int, number_of_chars_in_a_coupon: int, prefix: str = ""
+) -> None:
     if number_of_coupons > MAX_NUMBER_OF_COUPONS:
         raise ValueError(f"n cannot be greater than {MAX_NUMBER_OF_COUPONS}")
     elif number_of_coupons < MIN_NUMBER_OF_COUPONS:
@@ -74,6 +88,20 @@ def validate(number_of_coupons: int, number_of_chars_in_a_coupon: int) -> None:
         raise ValueError(f"l cannot be greater than {MAX_LENGTH_OF_COUPON}")
     elif number_of_chars_in_a_coupon < MIN_LENGTH_OF_COUPON:
         raise ValueError(f"l cannot be less than {MIN_LENGTH_OF_COUPON}")
+
+    number_of_chars_in_a_prefix = len(prefix)
+    if number_of_chars_in_a_prefix:
+        if number_of_chars_in_a_prefix > MAX_LENGTH_OF_PREFIX:
+            raise ValueError(
+                f"p cannot have more than {MAX_LENGTH_OF_PREFIX} character(s)"
+            )
+
+        pattern = f"^{ALLOWED_CHARACTERS_FOR_PREFIX}+$"
+        found = re.fullmatch(pattern, prefix)
+        if found is None:
+            raise ValueError(
+                f"p must only use the following characters {ALLOWED_CHARACTERS_FOR_PREFIX}"
+            )
 
 
 def generate(number_of_coupons: int, number_of_chars_in_a_coupon: int) -> Set[str]:
--- test_project.py
+++ test_project.py
@@ -4,6 +4,7 @@ from project import validate, generate, save
 
 def test_validate():
     assert validate(1, 5) == None
+    assert validate(1, 5, "CS50") == None
 
 
 def test_validate_should_raise_ValueError_when_n_is_less_than_1():
@@ -32,6 +33,20 @@ def test_validate_should_raise_ValueError_when_l_is_greater_than_16():
         validate(10, 10000)
 
     assert str(e.value) == "l cannot be greater than 16"
+
+
+def test_validate_should_raise_ValueError_when_p_has_more_than_16_char():
+    with pytest.raises(ValueError) as e:
+        validate(10, 10, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    assert str(e.value) == "p cannot have more than 16 character(s)"
+
+
+def test_validate_should_raise_ValueError_when_p_has_invalid_characters():
+    with pytest.raises(ValueError) as e:
+        validate(5, 5, "abc!@#$")
+
+    assert str(e.value) == "p must only use the following characters [A-Z0-9]"
 
 
 def test_generate():
